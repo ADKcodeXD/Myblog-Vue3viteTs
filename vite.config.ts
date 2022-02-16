b445d315b286608a25afde299455383fa5ec77ff
ADKcode <790532173@qq.com>
2022-02-16 17:51:54 +0800

    vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite';
 
 function resovePath(paths: string) {
   // 如何 __dirname 找不到 需要 yarn add @types/node --save-dev
@@ -8,7 +10,14 @@ function resovePath(paths: string) {
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),AutoImport({
+    dts:'src/auto-imports.d.ts',
+    imports:['vue','vue-router']
+  }),Components({
+    dirs:['src/components'],
+    extensions:['vue'],
+    dts:'src/components.d.ts'
+  })],
   resolve: {
     alias: {
       "@": resolve(__dirname, 'src')
