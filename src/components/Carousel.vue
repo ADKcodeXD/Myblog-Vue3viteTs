24bff82ed320f5bef90a74ea82de175728a54a71
ADKcode <790532173@qq.com>
2022-02-16 17:48:25 +0800

    基于ElementPlus轮播图组件封装

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,12 +1,33 @@
 <template>
-  <el-carousel>
-    <el-carousel-item v-for="item in 4" :key="item">
-      <h3>{{ item }}</h3>
+  <el-carousel pause-on-hover v-loading="items.length === 0">
+    <el-carousel-item v-for="item in items" :key="item.id">
+      <div class="content" @click="$router.push(`/article/${item.id}`)">
+        <h2 class="title">{{ item.articleName }}</h2>
+        <el-image :src="item.banner" fit="cover">
+          <template #placeholder>
+            <div class="image-slot">Loading<span class="dot">...</span></div>
+          </template>
+        </el-image>
+      </div>
     </el-carousel-item>
   </el-carousel>
 </template>
+<script lang="ts">
+import { Banner } from "@/interface/article";
+import { PropType } from "@vue/runtime-core";
 
-<style scoped>
+export default defineComponent({
+  props: {
+    items: {
+      type: Array as PropType<Banner[]>,
+      default: () => [],
+    },
+  },
+  setup(props) {},
+});
+</script>
+
+<style lang="less" scoped>
 .el-carousel__item h3 {
   color: #475669;
   font-size: 18px;
@@ -22,5 +43,25 @@
 
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
+}
+.content {
+  height: 300px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  .el-image {
+    width: 100%;
+    height: auto;
+    filter: brightness(0.7);
+  }
+  .title {
+    position: absolute;
+    bottom: 15px;
+    left: 20px;
+    text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.6);
+    font-size: 40px;
+    color: #fff;
+    z-index: 1;
+  }
 }
 </style>
