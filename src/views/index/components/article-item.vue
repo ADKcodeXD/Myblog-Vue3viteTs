98eccc7290c7bcc8d56e180908fbc5bd4c67c2a4
ADKcode <790532173@qq.com>
2022-02-16 17:51:37 +0800

    拆分 article-item组件

--- src/views/index/components/article-item.vue
+++ src/views/index/components/article-item.vue
@@ -1,27 +1,51 @@
 <template>
-  <div class="box">
+  <div class="box" @click="$router.push(`/article/${articleItem.id}`)">
     <div class="image">
-      <img src="@/assets/img/women.png" alt="" />
+      <img :src="articleItem.banner" alt="" />
     </div>
     <div class="content">
-      <p class="title">这是标题</p>
-      <p class="desc">
-        这是简介这是简介这是简介这是简 这是简介这是简介这是简介
-        这是简介这是简介这是简介 这是简介这是简介这是简介这是简
-        这是简介这是简介这是简介 这是简介这是简介这是简介介这简介
-        这是简介这是简介这是简介这是简介 这是简介这是简介这是简介
-        这是简介这是简介这是简介这是简介 这是简介这是简介这是简介
-        是简介这是简介这是简介这是简介 这是简介这是简介这是简介这
-      </p>
+      <div class="up">
+        <div class="left">
+          <p class="title">{{ articleItem.articleName }}</p>
+          <p class="desc">
+            {{ articleItem.summary }}
+          </p>
+        </div>
+        <div class="right">
+          <p class="time">{{ time }}</p>
+        </div>
+      </div>
+      <div class="down">
+        <div class="leftcontent">
+          <p class="author">作者:{{ articleItem.author }}</p>
+          <i class="iconfont icon-yanjing">阅读:{{ articleItem.viewCounts }}</i>
+          <i class="iconfont icon-pinglun">评论:{{ articleItem.commentCounts }}</i>
+        </div>
+        <TagsGroup :tags="articleItem.tags"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
+import { computed, defineComponent, PropType } from "vue";
+import { getRealativeTime } from "@/utils/dayjs";
+import TagsGroup from "@/components/TagsGroup.vue";
+import { ArticleItemInfo } from "@/interface/article";
 export default defineComponent({
-  setup() {},
+    props: {
+        articleItem: {
+            type: Object as PropType<ArticleItemInfo>,
+            default: () => { },
+        },
+    },
+    setup(props) {
+        const time = computed(() => {
+            return getRealativeTime(props.articleItem.createDate);
+        });
+        return { time };
+    },
+    components: { TagsGroup }
 });
 </script>
 
@@ -31,7 +55,7 @@ export default defineComponent({
   height: 150px;
   padding: 5px;
   margin: 10px 0;
-//   border-bottom: 1px solid rgb(216, 216, 216);
+  //   border-bottom: 1px solid rgb(216, 216, 216);
   display: flex;
   justify-content: start;
   align-items: center;
@@ -45,29 +69,65 @@ export default defineComponent({
     }
   }
   .content {
-    .title {
-      font-size: 32px;
-    }
-    .desc {
+    display: flex;
+    flex-direction: column;
+    align-self: flex-start;
+    justify-content: space-between;
+    height: 100%;
+    width: 100%;
+    .up {
+      display: flex;
+      justify-content: space-between;
+      .time {
+        padding: 5px;
+      }
+      .time {
+        justify-content: flex-end;
+      }
+      .title {
+        font-size: 32px;
+      }
+      .desc {
         margin-top: 5px;
-      color: gray;
-      font-size: 14px;
-      word-break: break-all;
-      text-overflow: ellipsis;
-      display: -webkit-box;
-      -webkit-box-orient: vertical;
-      -webkit-line-clamp: 4;
-      overflow: hidden;
+        color: gray;
+        font-size: 14px;
+        word-break: break-all;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 4;
+        overflow: hidden;
+      }
     }
-    
-  }
-  &:hover{
-        color: rgb(241, 201, 115) ;
-        background-color: rgb(248, 248, 248);
-        .desc{
-            color:  rgb(241, 201, 115);
+    .down {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+
+      .leftcontent {
+        display: flex;
+        align-items: center;
+        .author {
+          color: rgb(16, 95, 119);
+          font-size: 14px;
+          margin-right: 10px;
         }
-        cursor: pointer;
+        i{
+          color: rgb(16, 95, 119);
+          margin-right: 5px;
+        }
+      }
+
+      
     }
+  }
+  &:hover {
+    color: rgb(241, 201, 115);
+    background-color: rgb(248, 248, 248);
+    .desc {
+      color: rgb(241, 201, 115);
+    }
+    cursor: pointer;
+  }
 }
 </style>
