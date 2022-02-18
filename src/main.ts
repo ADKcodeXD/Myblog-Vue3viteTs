3172f3a0567f0c6da45e6e20386050d1b9863978
ADKcode <790532173@qq.com>
2022-02-18 16:11:08 +0800

    fix:顺序调整

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,4 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/assets/styles/iconfont.less'
 
-createApp(App).use(router).use(ElementPlus).use(createPinia()).mount('#app')
+createApp(App).use(ElementPlus).use(router).use(createPinia()).mount('#app')
