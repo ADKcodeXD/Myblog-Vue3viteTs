623c03e0267a29751a428fdfb0f6524be9a6cfaa
ADKcode <790532173@qq.com>
2022-02-18 16:06:44 +0800

    add:新增footer组件 纯静态

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="footer">
+     <p class="one">Made by adk</p>
+     <p class="two">仅供学习使用 本博客为个人用途 无任何盈利用途</p>
+     <p class="three">如果你也喜欢我的博客 欢迎你访问我的github 希望能够给个star哦~</p>
+ </div>
+</template>
+
+
+
+<style lang="less" scoped>
+.footer{
+    width: 100%;
+    height: 80px;
+    display: flex;
+    flex-direction: column;
+    color: #fff;
+    padding: 10px;
+    justify-content: center;
+    background-color: rgb(99, 99, 99);
+    margin-top: 20px;
+    .one{
+        font-size: 20px;
+    }
+    .two{
+        font-size: 12px;
+    }
+    .three{
+        font-size: 12px;
+    }
+}
+</style>
