fb7c92e3ab88b6cec71ed494717b07a253fa8b78
ADKcode <790532173@qq.com>
2022-02-18 16:05:42 +0800

    update:用户api新增

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,8 +1,9 @@
 // user/currentUser
+import { UpdateUserInfoParams } from '@/interface/params'
 import request from '@/utils/request'
 
 // 获取当前user信息
-export const currentUser = () => {
+export const currentUserApi = () => {
     return request({
         method: 'get',
         url: '/user/currentUser'
@@ -10,9 +11,26 @@ export const currentUser = () => {
 }
 
 // 获取指定用户信息
-export const findUserById = (id:any) => {
+export const findUserById = (id: any) => {
     return request({
         method: 'get',
         url: `/user/user?userId=${id}`
+    })
+}
+
+// 获取当前user的所有信息（除密码）
+export const currentUserAll = () => {
+    return request({
+        method: 'get',
+        url: '/user/currentUserAll'
+    })
+}
+
+// 修改登录用户的信息
+export const updateUserInfoApi = (updateParams: UpdateUserInfoParams) => {
+    return request({
+        method: 'post',
+        url: '/user/updateUserInfo',
+        data: updateParams
     })
 }
