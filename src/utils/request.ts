e925a526752236feff490eb78d8627f23f65d679
ADKcode <790532173@qq.com>
2022-02-18 16:11:53 +0800

    remove:移除Jsonbig 改为后端统一string

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,18 @@
 import { useStore } from './../store/main';
 import axios from 'axios'
-
+import JSONbig from 'json-bigint';
 
 const request: any = axios.create({
     baseURL: "http://localhost:8888",
-    timeout: 5000
+    timeout: 5000,
+    transformResponse: [function (data) {
+        try {
+          return JSONbig.parse(data)
+        } catch (err) {
+          return data
+        }
+    }]
+    
 })
 //请求拦截器  发请求之前，拦截器可以监测到
 request.interceptors.request.use((config: any) => {
