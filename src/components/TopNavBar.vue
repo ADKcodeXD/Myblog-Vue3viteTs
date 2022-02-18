d0ec60fc23df98b844f2fe7ec14341ea6b1a5fd4
ADKcode <790532173@qq.com>
2022-02-18 16:07:56 +0800

    fix:更改导航栏绑定 使用pinia双向绑定实时更新

--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="navbar" >
+  <div class="navbar">
     <!-- 导航组件 -->
-    <el-header :style="{backgroundColor:backColor}">
+    <el-header :style="{ backgroundColor: backColor }">
       <el-row :gutter="20">
         <el-col :span="4">
           <div class="icon">
@@ -12,10 +12,10 @@
         </el-col>
         <el-col :span="16">
           <div class="menu">
-            <ul class="menu-list" :style="{color:fontColor}">
+            <ul class="menu-list" :style="{ color: fontColor }">
               <li><a @click="$router.push('/')">首页</a></li>
               <li><a @click="$router.push('/articlelist')">文章</a></li>
-              <li><a>留言板</a></li>
+              <li><a @click="$router.push('/messageboard')">留言板</a></li>
               <li><a @click="$router.push('/project')">作品</a></li>
               <li @click="$router.push('/edit')">
                 <el-button :icon="Edit" circle></el-button><a>我也要写</a>
@@ -25,31 +25,47 @@
         </el-col>
         <el-col :span="4">
           <div class="right">
-            <p v-if="userinfo.id">{{ userinfo.username }} 你好</p>
-            <p v-else>访客你好 去<RouterLink to="/login">登录</RouterLink></p>
+            <p v-if="userStore.userinfo.id">{{ userStore.userinfo.nickname }} 你好</p>
+            <p v-else>
+              访客你好 去<RouterLink
+                to="/login"
+                style="color: rgb(255, 226, 226)"
+                >登录</RouterLink
+              >
+            </p>
             <div
               class="container"
               @mouseenter="infoShow = true"
               @mouseleave="infoShow = false"
             >
               <div class="avatar">
-                <img v-if="userinfo.id" :src="userinfo.avatar" alt="" />
-                <img v-else src="@/assets/img/94551956_p0.png" alt="" />
+                <img v-if="userStore.userinfo.id" :src="userStore.userinfo.avatar" alt="" />
+                <img v-else src="@/assets/img/logo.png" alt="" />
               </div>
               <transition name="infobox" mode="out-in">
-                <div v-show="infoShow" class="info-box">
-                  <div class="loginbox" v-if="userinfo.id">
+                <div
+                  v-show="infoShow"
+                  class="info-box"
+                  
+                >
+                  <div class="imgcontainer" :style="{ backgroundImage: `url(${userStore.userinfo.banner})` }"></div>
+                  <div class="loginbox" v-if="userStore.userinfo.id">
                     <div class="up">
                       <div class="avatar">
-                        <img src="@/assets/img/94551956_p0.png" alt="" />
+                        <img :src="userStore.userinfo.avatar" alt="" />
                       </div>
                       <div class="info">
-                        <h2>欢迎您{{ userinfo.username }}</h2>
-                        <div>你的角色是{{ userinfo.role }}</div>
+                        <h2>{{ userStore.userinfo.nickname }}</h2>
+                        <div>{{ userStore.userinfo.introduce }}</div>
                       </div>
                     </div>
                     <div class="button">
-                      <el-button type="primary" round>编辑资料</el-button>
+                      <el-button
+                        type="primary"
+                        @click="$router.push('/home')"
+                        round
+                        >编辑资料</el-button
+                      >
                       <el-button type="danger" @click="logout" round
                         >退出登录</el-button
                       >
@@ -71,19 +87,19 @@ import { useStore } from "@/store/main";
 import { removeItem } from "@/utils/storage";
 import { Edit } from "@element-plus/icons-vue";
 import { ElMessage } from "element-plus";
-import { currentUser } from "@/api/user";
+import { currentUserApi } from "@/api/user";
 import { useUserStore } from "@/store/user";
 import { isValidKey } from "@/utils/tools";
 const props = defineProps({
-    backColor:{
-        type:String,
-        default:()=>'#ff9f9f'
-    },
-    fontColor:{
-      type:String,
-      default:()=>'#fff'
-    }
-})
+  backColor: {
+    type: String,
+    default: () => "#ff9f9f",
+  },
+  fontColor: {
+    type: String,
+    default: () => "#fff",
+  },
+});
 let infoShow = ref(false);
 const store = useStore();
 const userStore = useUserStore();
@@ -91,43 +107,26 @@ const user = store.user;
 const getUserInfo = () => {
   if (user && user.token) {
     // 获取信息
-    currentUser()
+    currentUserApi()
       .then((result: any) => {
-        const {data}=result;
+        const { data } = result;
         if (data.code === 10003) {
           ElMessage.error(data.msg);
           removeItem("user");
         } else {
           userStore.setUser(data.data);
-          for(let key in userinfo){
-            // 此处直接使用key会报错 需要使用这个isValidKey函数来进行判断
-            if(isValidKey(key,userinfo)){
-              userinfo[key]=userStore.userinfo[key];
-            }
-          }
         }
       })
       .catch((error: any) => {
         ElMessage.error("请求失败");
       });
-    
   } else {
     console.log("notoken");
   }
 };
 getUserInfo();
-interface userinfo{
-  id:string,
-  username:string,
-  role:string,
-  avatar:string
-}
-let userinfo:userinfo=reactive({
-  id:'',
-  username:'',
-  role:'',
-  avatar:''
-});
+
+
 
 const logout = (): void => {
   store.user.token = "";
@@ -137,14 +136,17 @@ const logout = (): void => {
     username: "",
     avatar: "",
     role: "",
+    introduce: "",
+    banner: "",
+    nickname: "",
   });
-  for(let key in userinfo){
-    if(isValidKey(key,userinfo)){
-      userinfo[key]=userStore.userinfo[key];
+  for (let key in userinfo) {
+    if (isValidKey(key, userinfo)) {
+      userinfo[key] = userStore.userinfo[key];
     }
   }
   ElMessage.success("退出登录成功！");
-  infoShow.value=false;
+  infoShow.value = false;
 };
 </script>
 
@@ -167,7 +169,7 @@ const logout = (): void => {
 .el-header {
   padding: 0;
   height: 93px;
-  
+
   .el-row {
     height: 100%;
     padding: 10px;
@@ -217,10 +219,21 @@ const logout = (): void => {
           z-index: 999;
           background-color: rgb(253, 253, 253);
           box-shadow: -2px 2px 5px rgba(0, 0, 0, 0.3);
+          background-size: 100% auto;
+          .imgcontainer{
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            z-index: -5;
+            opacity: 0.8;
+            filter: blur(1px) brightness(0.6);
+            background-size: cover;
+            
+          }
           .loginbox {
             width: 100%;
             display: flex;
-            color: rgb(0, 0, 0);
+            color: rgb(250, 250, 250);
             padding: 10px 0;
             font-size: 14px;
             flex-direction: column;
@@ -262,6 +275,7 @@ const logout = (): void => {
           }
 
           img {
+            height: 100%;
             width: 100%;
             object-fit: cover;
           }
