ff03ab4f683201806fa43c8d5af2751247213127
ADKcode <790532173@qq.com>
2022-02-19 23:49:59 +0800

    主题文件声明

---
+++ src/interface/theme.type.ts
@@ -0,0 +1,13 @@
+export interface Theme{
+    primaryBackGroundColor: string;
+    primaryTextColor:string;
+    primaryActiveColor:string;
+    primaryActiveTextColor:string;
+    primaryTipColor:string;
+    primaryTagTextColor:string;
+    primarySubColor:string;
+    primaryTagBackGroundColor:string;
+    primaryHightlightColor:string;
+    primaryLinkColor:string;
+    primaryHoverColor:string;
+}
