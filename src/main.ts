ee36bac9e8e2dd2244e0b53fe6e561f6c37f5d73
ADKcode <790532173@qq.com>
2022-02-23 11:17:24 +0800

    add:使用插件

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/assets/styles/iconfont.less'
+import '@/assets/styles/github-light.css'
+import 'highlight.js/lib/common'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
+import 'highlight.js/styles/github.css'
 
-
-
-createApp(App).use(ElementPlus).use(createPinia()).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(createPinia()).use(router).use(hljsVuePlugin).mount('#app')
 
 
