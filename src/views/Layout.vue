bef2e3be4c14fd407397e042f2f02f70bafe0b0e
ADKcode <790532173@qq.com>
2022-03-15 22:50:15 +0800

    update:增加keepalive组件缓存

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,11 +3,16 @@
     <!-- 头部 -->
     <TopNavBar />
     <!-- banner -->
-    <div class="main" >
+    <div class="main">
       <!-- 导航 -->
       <!-- routerview -->
-      <Skin  />
-      <router-view></router-view>
+      <Skin />
+      <!-- vue3.0配置 -->
+      <router-view v-slot="{ Component }">
+        <keep-alive :include="pages">
+          <component :is="Component"></component>
+        </keep-alive>
+      </router-view>
     </div>
     <!-- 底部 -->
     <div class="boxgroup">
@@ -23,8 +28,20 @@
 
 <script setup lang="ts">
 import TopNavBar from "@/components/TopNavBar.vue";
-import { useThemeStore } from "@/store/theme";
+import { useCacheStore } from "@/store/cache";
 import { setTheme } from "@/theme/theme";
+const cachePages = useCacheStore();
+const pages = cachePages.cachePages;
+const route = useRoute();
+
+// 监视页面 并动态设置页面缓存
+watch(route, (to) => {
+  if (to.meta.keepAlive) {
+    if (to.name) {
+      cachePages.setCachePage(to.name.toString());
+    }
+  }
+});
 
 onMounted(() => {
   setTheme();
@@ -32,5 +49,5 @@ onMounted(() => {
 </script>
 
 <style scoped  lang="less">
-@import url('@/assets/styles/pc/layout-pc.less');
+@import url("@/assets/styles/pc/layout-pc.less");
 </style>
