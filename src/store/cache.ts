29e7c3d03c95f093ca5f9a3ee1dacd29dde4919a
ADKcode <790532173@qq.com>
2022-03-15 22:51:34 +0800

    add:新增keepalive动态缓存store

---
+++ src/store/cache.ts
@@ -0,0 +1,30 @@
+import { getItem } from '@/utils/storage';
+import { defineStore } from 'pinia'
+
+export const useCacheStore = defineStore('cache', {
+    state: () => {
+        return {
+            // 这里写变量
+            cachePages: [] as Array<string>
+        }
+    },
+    // could also be defined as
+    // state: () => ({ count: 0 })
+    actions: {
+        // 这里写方法action
+        setCachePage(page: string) {
+            if (!this.cachePages.includes(page)) {
+                this.cachePages.push(page)
+
+            }
+        },
+        removeCachePages(page:string){
+            if(this.cachePages.indexOf(page)!==-1){
+                this.cachePages.splice(this.cachePages.indexOf(page),1);
+            }
+        }
+    },
+    getters: {
+        //   getters
+    }
+})
