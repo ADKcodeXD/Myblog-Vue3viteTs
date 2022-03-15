835ace7992b1cc22caa8b562553a700fc9194dc0
ADKcode <790532173@qq.com>
2022-03-15 22:47:18 +0800

    add:keep-alive尝试

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
 <template>
+
   <router-view></router-view>
+
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {useCacheStore}  from '@/store/cache'
+export default defineComponent({
+  setup() {
+    const cacheStore=useCacheStore();
+    const pages=cacheStore.cachePages;
+    return {
+      pages
+    }
+  },
+})
+</script>
 
