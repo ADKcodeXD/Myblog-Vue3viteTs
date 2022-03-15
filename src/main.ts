2dc90560d5dfe7f23ed1f85e97cdb174bf73c47b
ADKcode <790532173@qq.com>
2022-03-15 22:49:30 +0800

    fix:element本地化引入

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import '@/assets/styles/iconfont.less'
 import '@/assets/styles/github-light.css'
@@ -10,6 +11,6 @@ import 'highlight.js/lib/common'
 import hljsVuePlugin from '@highlightjs/vue-plugin'
 import 'highlight.js/styles/github.css'
 
-createApp(App).use(ElementPlus).use(createPinia()).use(router).use(hljsVuePlugin).mount('#app')
+createApp(App).use(ElementPlus,{locale: zhCn,}).use(createPinia()).use(router).use(hljsVuePlugin).mount('#app')
 
 
