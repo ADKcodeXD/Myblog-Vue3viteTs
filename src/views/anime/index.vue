d99e059bf5142ab0a70c52b7f89774e51d9c8c85
ADKcode <790532173@qq.com>
2022-03-21 00:11:17 +0800

    add: 动漫资讯页

--- src/views/anime/index.vue
+++ src/views/anime/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="tw-w-4/5 tw-mx-auto">
-      <AnimeHeader/>
-      <AnimeCarousel/>
-      <AnimeBroswer/>
+  <div class="tw-w-fill tw-px-5 tw-mt-28 lg:tw-w-4/5 lg:tw-mx-auto lg:tw-mt-0">
+    <AnimeHeader />
+    <AnimeCarousel />
+    <div class="tw-flex tw-mt-10 tw-flex-col md:tw-flex-row">
+      <div class="left md:tw-w-9/12">
+        <AnimeBroswer />
+      </div>
+      <div class="right md:tw-flex-1 md:tw-ml-4">
+          <AnimeMinagamiteiru />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,12 +18,13 @@ import { defineComponent } from "vue";
 import AnimeHeader from "./components/AnimeHeader.vue";
 import AnimeCarousel from "./components/AnimeCarousel.vue";
 import AnimeBroswer from "./components/AnimeBroswer.vue";
+import AnimeMinagamiteiru from "./components/AnimeMinagamiteiru.vue";
 
 export default defineComponent({
-    setup() {
-        return {};
-    },
-    components: { AnimeHeader, AnimeCarousel,AnimeBroswer }
+  setup() {
+    return {};
+  },
+  components: { AnimeHeader, AnimeCarousel, AnimeBroswer, AnimeMinagamiteiru },
 });
 </script>
 
