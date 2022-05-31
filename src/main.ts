a5474df79f87237554715b3b0df679abfac73336
ADKcode <790532173@qq.com>
2022-05-31 22:39:12 +0800

    optimization：Introduce on demand

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
-import ElementPlus from 'element-plus'
+import ElementPlus from 'element-plus';
+// 本地化 elementplus
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
+// 引入iconfont
 import '@/assets/styles/iconfont.less'
-import '@/assets/styles/github-light.css'
 import 'highlight.js/lib/common'
 import hljsVuePlugin from '@highlightjs/vue-plugin'
-import 'highlight.js/styles/github.css'
+// tailwind 的css文件 
 import '@/assets/styles/index.css'
-import * as echarts from 'echarts'
 import '@/assets/styles/preflight.css';
-import 'vue3-video-play/dist/style.css' // 引入css
+import 'virtual:svg-icons-register';
 
 const app=createApp(App).use(ElementPlus,{locale: zhCn,}).use(createPinia()).use(router).use(hljsVuePlugin)
-app.config.globalProperties.$echarts = echarts;
+// app.config.globalProperties.$echarts = echarts;
 app.mount('#app')
