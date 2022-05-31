f395948dac17b5c43cf5a8a35ae23bb4d1044b9d
ADKcode <790532173@qq.com>
2022-05-31 22:28:26 +0800

    add:new components adkbutton and adk divider custom components

---
+++ src/components/AdkDivider/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="divider">
+    </div>
+</template>
+<script setup lang="ts">
+const props=defineProps({
+    height:{
+        type:Number,
+        deault:20
+    }
+})
+</script>
+<style lang="less" scoped>
+.divider{
+    background: url(@/assets/img/divirer.png);
+    width: 100%;
+    height: 20px;
+    margin: 3px;
+    background-size: contain;
+}
+</style>
