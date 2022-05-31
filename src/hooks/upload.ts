2e5556e66e93dd8b4114b0ae40044188196af2ce
ADKcode <790532173@qq.com>
2022-05-31 22:38:44 +0800

    fix:Element type bug in uploadFile

--- src/hooks/upload.ts
+++ src/hooks/upload.ts
@@ -2,33 +2,32 @@ import { uploadBanner } from "@/api/article";
 import { ElMessage } from "element-plus";
 import { ElFile, UploadFile } from "element-plus/es/components/upload/src/upload.type";
 
-export const useUpload = async (file: UploadFile): Promise<string> => {
+export const useUpload = async (file: ElFile): Promise<string> => {
     const isImg = file.type === "image/jpeg" || file.type === "image/png";
     const isLt5M = file.size / 1024 / 1024 < 5;
-    let flag: string = "";
     if (!isImg) {
         ElMessage.error("请上传jpg或者png格式的图片");
-        return flag;
+        return "";
     }
     if (!isLt5M) {
         ElMessage.error("请不要上传大于5MB的图片");
-        return flag;
+        return "";
     }
     // 上传到后端
     const fd = new FormData();
     fd.append("image", file);
+    let url = ""
     try {
         const { data } = await uploadBanner(fd);
         if (data.code !== 200) {
             ElMessage.error(data.msg);
-            return flag;
+            return url;
         } else {
-            ElMessage.success("上传成功");
-            flag=data.data;
+            url = data.data;
         }
     } catch (error) {
         ElMessage.error("上传发生错误");
     }
 
-    return flag;
+    return url;
 }
