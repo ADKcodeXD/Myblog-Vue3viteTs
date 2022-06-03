d04922329e2ed8984050e93d307dd46e2230ef84
ADKcode <790532173@qq.com>
2022-06-03 13:26:10 +0800

    add:新增动态加载动画 css实现

---
+++ src/components/LoadingAnime/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="tw-flex tw-w-full tw-h-full tw-items-center tw-justify-center">
+        <div class="loading-box" style="--i:1;"></div>
+        <div class="loading-box" style="--i:2;"></div>
+        <div class="loading-box" style="--i:3;"></div>
+    </div>
+</template>
+
+<style lang="less" scopd>
+@keyframes uptodown {
+
+    0%,
+    50%,
+    100% {
+        transform: translateY(0);
+    }
+
+    25% {
+        transform: translateY(-15px);
+    }
+
+    75% {
+        transform: translateY(15px);
+    }
+}
+
+.loading-box{
+    .ball(1rem, #fc566c);
+    margin: 1rem;
+    animation: uptodown 1s linear calc(var(--i)*0.15s) infinite both;
+}
+</style>
