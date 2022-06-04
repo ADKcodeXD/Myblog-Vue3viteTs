487932f2755632565c3cdca2c2a071251c9b5eca
ADKcode <790532173@qq.com>
2022-06-05 00:36:29 +0800

    Refactor:抽取分页组件

---
+++ src/components/MyPagination/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <ElPagination background layout="prev, pager, next" :page-count="Math.ceil(total / pageparams.pagesize)"
+      @current-change="changePage" :current-page="pageparams.page">
+    </ElPagination>
+</template>
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+const props=defineProps({
+    pageparams:{
+        type:Object as PropType<PageParams>,
+        default:()=>{}
+    },
+    total:{
+        type:Number,
+        default:0
+    }
+})
+const emit=defineEmits(['changePage'])
+const changePage=(val:number)=>{
+    props.pageparams.page = val;
+    emit('changePage')
+}
+</script>
+<style lang="less" scoped>
+    
+</style>
