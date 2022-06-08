4896854576ed0d276f36e2c90665e3b023586410
ADKcode <790532173@qq.com>
2022-06-09 00:44:23 +0800

    tools:增加 驼峰转换横杠线的方法

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,6 +1,25 @@
-export function isValidKey(
-    key: string | number | symbol,
-    object: object
-): key is keyof typeof object {
+export function isValidKey(key : string | number | symbol, object : object): key is keyof typeof object {
     return key in object;
 }
+/**
+ * 用于将驼峰命名法的变量转换为横杠线连接的命名
+ * @param val 一个驼峰的string属性
+ */
+export const humpToLine= (val : string) => {
+    if (val === val.toLowerCase()) { // 全是小写 不需要转换
+        return val;
+    } else {
+        let res = ""
+        for (let i = 0; i < val.length; i++) {
+            if (val.charCodeAt(i) > 65 && val.charCodeAt(i)<90){
+                res+=`-${val[i].toLowerCase()}`;
+            } 
+            else{
+                res+=val[i];
+            }
+        }
+        return res;
+    }
+}
+
+
