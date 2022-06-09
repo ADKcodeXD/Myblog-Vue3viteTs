785fe9d4ce6c7fb692cf8a2f833a27b59ddd7965
ADKcode <790532173@qq.com>
2022-06-10 01:00:25 +0800

    theme:add two style method

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,17 +1,19 @@
-import { humpToLine } from './../utils/tools';
+import {humpToLine} from './../utils/tools';
 import {getItem, setItem} from '@/utils/storage';
-import {defaultConfig, themes} from "./themeData";
+import {darkConfig, defaultConfig, themes} from "./themeData";
 // 修改页面中的样式变量值
 const changeStyle = (obj : Theme) => {
     let style: HTMLStyleElement;
-    if (document.head.getElementsByTagName('style')[0]!==null){
-      style=document.head.getElementsByTagName('style')[0];
-    }else{
-      style = document.createElement('style');
+    if (document.head.getElementsByTagName('style')[0] !== null) {
+        style = document.head.getElementsByTagName('style')[0];
+    } else {
+        style = document.createElement('style');
     }
-    let str="";
+    let str = "";
     for (let key in obj) {
-        str+=`--${key}:${obj[key]};
+        str += `--${key}:${
+            obj[key]
+        };
         `
     }
     style.innerHTML = `body:root{
@@ -48,18 +50,41 @@ export const setTheme = (themeName : string | void) => {
         }
     }
 };
-
 /**
  * 用于getlocalitem设置整体的style属性
  * @param config type : GlobalConfig 要求类型是一个全局配置项
  */
-const setProperty=(config:GlobalConfig)=>{
-    for(let key in config){
-        // 由于命名原因 如果是number类型的config 则直接转换成横线
-        if(typeof config[key] ==='number'){
-            document.documentElement.style.setProperty(`--${humpToLine(key)}`,`${config[key]}rem`);
-        }else{
-            document.documentElement.style.setProperty(`--${key}`,config[key]);
+const setProperty = (config : GlobalConfig) => {
+    for (let key in config) { // 由于命名原因 如果是number类型的config 则直接转换成横线
+        if (typeof config[key] === 'number') {
+            if (key === 'backGroundImgLight') {
+                document.documentElement.style.setProperty(`--${key}`, `${
+                    config[key]
+                }%`);
+            } else if (key === 'backGroundImgBlur') {
+                document.documentElement.style.setProperty(`--${key}`, `${
+                    config[key]
+                }rem`);
+            } else {
+                document.documentElement.style.setProperty(`--${
+                    humpToLine(key)
+                }`, `${
+                    config[key]
+                }rem`);
+            }
+        } else {
+            if (key === 'backGroundImg') {
+                try {
+                    let container: HTMLElement = document.getElementsByClassName('cotainer')[0] as HTMLElement;
+                    container.style.backgroundImage = `url(${
+                        config[key]
+                    })`;
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                document.documentElement.style.setProperty(`--${key}`, config[key]);
+            }
         }
     }
 }
@@ -68,25 +93,38 @@ const setProperty=(config:GlobalConfig)=>{
  * @param varName css属性变量的名字
  * @param val 属性的值 可以为number或者string
  */
-const setOneProperty=(varName:string,val:string|number)=>{
-
-}
+const setOneProperty = (varName : string, val : string | number) => {}
 /**
  * 可以自适应的传入或者不传入 不传入则默认读取本地的配置
- * 传入配置会自动保存本地
+ * 传入local 配置则保存本地
  * @param config 一个GlobalConfig 或者不传 
+ * @param local Boolean 默认为false 用于保存本地
  */
-export const setConfig=(config:GlobalConfig|void)=>{
-    if(!config){
-        let localConfig=getItem('globalConfig');
-        if(!localConfig){
+export const setConfig = (config : GlobalConfig | void, local : Boolean = false) => {
+    if (!config) {
+        let localConfig = getItem('globalConfig');
+        if (! localConfig) {
             setProperty(defaultConfig);
-            setItem('globalConfig',defaultConfig);
-        }else {
+            setItem('globalConfig', defaultConfig);
+        } else {
             setProperty(localConfig);
         }
-    }else{
+    } else {
         setProperty(config);
-        setItem('globalConfig',config);
+        if (local) { // 如果不传 则不保存到本地
+            setItem('globalConfig', config);
+        }
     }
+}
+/**
+ * 用于将设置重置回默认设置
+ */
+export const setDefault = () => {
+    setConfig(defaultConfig, true);
+}
+/**
+ * 用于将配置设置为夜间模式
+ */
+export const setDark = () => {
+    setConfig(darkConfig, true);
 }
