1c990b9fd86834ba3a7dc6ab545643016485149d
ADKcode <790532173@qq.com>
2022-06-10 00:55:52 +0800

    style:update footer styles

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,36 +1,46 @@
 <template>
- <div class="footer">
-     <div class="left">
-         <p class="one">Made by adk</p>
-        <p class="two">仅供学习使用 本博客为个人用途 无任何盈利用途</p>
-     </div>
-     
-     <div class="right">
-         备案/许可证编号：粤ICP备2022022555号
-     </div>
- </div>
+    <div class="footer">
+        <div class="left">
+            <p class="one">Adk-blog</p>
+            <p class="two">仅供学习使用 本博客为个人用途 无任何盈利用途</p>
+            <p class="three">备案/许可证编号：粤ICP备2022022555号</p>
+        </div>
+        <div class="right">
+            <p>本项目基于MIT Licence进行开源</p>
+            <!-- <p>我的联系方式:790532173@qq.com</p> -->
+            <p>项目地址:<a href="" target="_blank">Github</a></p>
+            <p>Copyright © 2022 adkdream.top</p>
+        </div>
+    </div>
 </template>
 
 
 
 <style lang="less" scoped>
-.footer{
+.footer {
     width: 100%;
     display: flex;
-    color: #fff;
-    padding: 10px;
+    color: @textColor;
+    padding: 0.5rem 5rem;
     justify-content: space-between;
-    align-items: flex-end;
-    background-color: rgb(99, 99, 99);
+    align-items: center;
+    background-color: @bgColor;
     margin-top: 20px;
-    .one{
-        font-size: 20px;
+    .shadow(-1px, 2px);
+    .font-normal();
+    .one {
+        font-size: @big-text;
     }
-    .two{
-        font-size: 12px;
+    .two {
+        font-size: @small-text;
     }
-    .three{
-        font-size: 12px;
+
+    .three {
+        font-size: @very-samll-text;
+    }
+    .right{
+        font-size: @very-samll-text;
+        text-align: right;
     }
 }
 </style>
