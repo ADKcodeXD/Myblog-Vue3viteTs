c14ef2ccad0af904d6c8b79abdcf510b48f71658
ADKcode <790532173@qq.com>
2022-06-18 00:34:21 +0800

    update:对于首页的smallanimeitem进行可选属性和兼容性改进

--- src/components/SmallAnimeItem/SmallAnimeItem.vue
+++ src/components/SmallAnimeItem/SmallAnimeItem.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="card-little" @click="$router.push(`/index/animedetail/${item.id}`)">
+  <div class="card-little" v-if="myItem.id" @click="$router.push(`/index/animedetail/${myItem.id}`)">
     <div class="card-little-img tw-w-2/5">
-      <MyElimage :img="item.images.common" :not-found-type="'3:4'" />
+      <MyElimage :img="myItem.images.common || ''" :not-found-type="'3:4'" />
     </div>
     <div class="card-little-info">
       <div>
         <p class="card-little-info__title">
-          {{ item.name_cn ? item.name_cn : item.name }}
+          {{ myItem.name_cn ? myItem.name_cn : myItem.name }}
         </p>
         <p class="card-little-info__tip">
-          综合评分{{ item.rating.score }}({{item.rating.total}}条评价)
+          综合评分{{ myItem.rating.score }}({{ myItem.rating.total }}条评价)
         </p>
       </div>
       <div>
-        <p class="card-little-info__rank">排名:{{item.rank}}</p>
-        <p class="card-little-info__miru">{{ item.collection.doing }}人在看</p>
+        <p class="card-little-info__rank">排名:{{ myItem.rank }}</p>
+        <p class="card-little-info__miru" v-if="myItem.collection?.doing">{{ myItem.collection.doing }}人在看</p>
       </div>
     </div>
   </div>
@@ -23,12 +23,37 @@
 <script lang="ts" setup>
 import { PropType } from "vue";
 
-defineProps({
+const props = defineProps({
   item: {
     type: Object as PropType<Bangumi.AnimeItemInfo>,
     default: {},
   },
+  broswerItem: {
+    type: Object as PropType<Bangumi.BroswerResult>,
+    default: {},
+  }
 });
+const myItem = reactive<any>({});
+onMounted(() => { 
+  if (props.item.id) {
+    for (let i in props.item) {
+      myItem[i] = props.item[i];
+    }
+  } else if (props.broswerItem) {
+    myItem.images = {};
+    myItem.images.common = props.broswerItem.imageUrl;
+    myItem.name_cn = props.broswerItem.nameCn;
+    myItem.name = props.broswerItem.name;
+    myItem.rating = { total: 0, score: 0, count: null };
+    myItem.rating.score = props.broswerItem.score;
+    myItem.rating.total = props.broswerItem.count;
+    myItem.rank = props.broswerItem.rank;
+    myItem.id = props.broswerItem.id;
+  } else{
+    console.log(123456);
+  }
+})
+
 </script>
 
 <style lang="less" scoped>
