3a5f8f5bfe8ff3973e09041ed9440a464f038f85
ADKcode <790532173@qq.com>
2022-06-26 22:24:51 +0800

    perf: ⚡️ remove global directive ,register in components

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import {createPinia} from 'pinia';
 // import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
 import '@/assets/styles/iconfont/iconfont.less'
-import hljs from 'highlight.js';
 import '@/assets/styles/tailwind/index.css'
 import '@/assets/styles/tailwind/preflight.css';
 import 'virtual:svg-icons-register';
@@ -14,14 +13,5 @@ const head = createHead();
 const pinia=createPinia();
 const app = createApp(App).use(router).use(pinia).use(head);
 // app.config.globalProperties.$echarts = echarts;
-// highlight js 的自定义指令
-app.directive('highlight', {
-    mounted(el) {
-        hljs.configure({});
-        let blocks = el.querySelectorAll('pre');
-        blocks.forEach((block) => {
-            hljs.highlightBlock(block)
-        })
-    }
-})
+
 app.mount('#app')
--- src/views/article/components/ArticleBody.vue
+++ src/views/article/components/ArticleBody.vue
@@ -2,18 +2,35 @@
     <section>
         <div v-html="html" v-highlight class="markdown-body" @click="ImagePreview($event)"></div>
         <template v-if="showPreview">
-            <ElImageViewer v-if="showPreview"  :infinite="false"
-                :url-list="urlList" :hide-on-click-modal="true" :teleported="false"
-                @close="close"
-                >
+            <ElImageViewer v-if="showPreview" :infinite="false" :url-list="urlList" :hide-on-click-modal="true"
+                :teleported="false" @close="close">
             </ElImageViewer>
         </template>
     </section>
 </template>
+
+<script lang="ts">
+export default {
+    directives: {
+        // 在模板中启用
+        highlight: {
+            mounted(el) {
+                hljs.configure({});
+                let blocks = el.querySelectorAll('pre');
+                blocks.forEach((block) => {
+                    hljs.highlightBlock(block)
+                })
+                console.log(blocks);
+            }
+        }
+    }
+}
+</script>
 <script setup lang="ts">
 import { isMobile } from '@/utils/mobile';
 import '@/assets/styles/markdown/markdown.less';
 import 'highlight.js/styles/monokai.css';
+import hljs from 'highlight.js';
 const prop = defineProps({
     html: {
         type: String,
@@ -22,26 +39,27 @@ const prop = defineProps({
 })
 const ImagePreview = (e) => {
     if (e.target.nodeName === 'IMG') {
-        urlList.value[0]=e.target.currentSrc;
-        showPreview.value=true;
-        isMobile()?document.documentElement.style.overflow='hidden':document.body.style.overflow='hidden';     
+        urlList.value[0] = e.target.currentSrc;
+        showPreview.value = true;
+        isMobile() ? document.documentElement.style.overflow = 'hidden' : document.body.style.overflow = 'hidden';
     }
 }
-const close=()=>{
-    showPreview.value=false;
-    isMobile()?document.documentElement.style.overflow='':document.body.style.overflow='';
+const close = () => {
+    showPreview.value = false;
+    isMobile() ? document.documentElement.style.overflow = '' : document.body.style.overflow = '';
 }
-let urlList=ref([]);
-let showPreview=ref(false);
+let urlList = ref([]);
+let showPreview = ref(false);
+
 </script>
+
 <style lang="less" scoped>
 .markdown-body {
     margin: @margin-general 0;
     padding: @padding-general;
 }
 
-.img{
+.img {
     height: unset !important;
 }
-
 </style>
