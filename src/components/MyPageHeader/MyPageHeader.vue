b83231c753addac72366c32c2b1b8fdeb1428886
ADKcode <790532173@qq.com>
2022-07-01 23:54:17 +0800

    refactor: 💡 抽取通用头部header

---
+++ src/components/MyPageHeader/MyPageHeader.vue
@@ -0,0 +1,47 @@
+<template >
+    <section class="header">
+        <div class="back-btn" @click="backFn">
+            <i class="iconfont icon-left"></i>
+            Back
+        </div>
+        <p class="page-title">
+            <slot>
+
+            </slot>
+        </p>
+    </section>
+</template>
+<script setup lang="ts">
+const { backFn } = withDefaults(defineProps<{
+    backFn?: (payload: MouseEvent) => void
+}>(), {
+    backFn: function () {
+        window.history.back();
+    }
+});
+</script>
+<style lang="less" scoped>
+.header {
+    margin-top: @margin-general;
+    display: flex;
+    align-items: center;
+    .back-btn {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        background-color: @bgColor;
+        padding: 0.5rem @padding-general ;
+        cursor: pointer;
+        .border-normal();
+
+        &:hover {
+            background-color: @hoverColor;
+            color: @hoverTextColor;
+        }
+    }
+    .page-title{
+        margin-left: 2rem;
+        font-size: @big-text;
+    }
+}
+</style>
