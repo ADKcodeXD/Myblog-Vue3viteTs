182ed40592ab7d0d557b8be31132a48aec65760c
ADKcode <790532173@qq.com>
2022-07-02 18:38:58 +0800

    feat: 🎉 新增api 画廊管理和登出api

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,6 +1,10 @@
 import request from '@/utils/request'
 
-
+/**
+ * 用户登录api
+ * @param loginParams 用户登录参数
+ * @returns 
+ */
 export const userLogin = (loginParams:any) => {
     return request({
         method: 'post',
@@ -8,11 +12,25 @@ export const userLogin = (loginParams:any) => {
         data:loginParams
     })
 }
-
+/**
+ * 用户注册 
+ * @param registerParams 用户注册参数
+ * @returns 
+ */
 export const userRegister = (registerParams:any) => {
     return request({
         method: 'post',
         url: '/api/register',
         data:registerParams
+    })
+}
+/**
+ * 用户登出 用于删除redis中的缓存信息
+ * @returns 
+ */
+export const userLogout = () => {
+    return request({
+        method: 'post',
+        url: '/api/login/logout',
     })
 }
--- src/api/pic.ts
+++ src/api/pic.ts
@@ -14,7 +14,43 @@ export const getPics = (pageParams : PageParams) => {
 export const addPic = (picParams : PicParams) => {
     return request({method: 'post', url: '/api/pic/addPic', data: picParams})
 }
+/**
+ * 点赞图片 无需登录 一分钟最多20次
+ * @param picId 图片id
+ * @returns 
+ */
+export const likePic = (picId : string) => {
+    return request({
+        method: 'get',
+        url: '/api/pic/likePic',
+        params: {
+            id: picId
+        }
+    })
+}
 
-export const likePic=(picId:string)=>{
-    return request({method: 'get', url: '/api/pic/likePic', params:{id:picId}})
+/**
+ * 修改图片信息接口
+ * @param picParams 修改图片的参数
+ * @returns 
+ */
+export const updateMyPic = (picParams : PicParams) => {
+    return request({method: 'post', url: '/api/pic/updateMyPic', data: picParams})
+}
+
+/**
+ * 删除自己发布的图片
+ * @param id 图片的id
+ * @returns 
+ */
+export const deleteMyPic = (id : string | number) => {
+    return request({method: 'post', url: `/api/pic/deleteMyPic/${id}`})
+}
+/**
+ * 获取发布的图片
+ * @param pageParams 分页参数 获取自己发布的图片
+ * @returns 
+ */
+export const getMyPic = (pageParams : PageParams) => {
+    return request({method: 'post', url: `/api/pic/getMyPics`, data: pageParams})
 }
