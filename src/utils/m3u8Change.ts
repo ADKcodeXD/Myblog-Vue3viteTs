24b6fe3dbfdb2b6b81cd87419dc80ad6c823392e
ADKCodeXD <790532173@qq.com>
2023-03-25 01:15:39 +0800

    fix: dont need trans partten

--- src/utils/m3u8Change.ts
+++ src/utils/m3u8Change.ts
@@ -10,16 +10,22 @@ export const transformM3u8Utils = (videoUrl: string): Promise<string> => {
         filereader.onload = () => {
           const result = filereader.result as string
           const arr = result.split('\n')
+          let count = 0
           const newarr = arr.map(item => {
             if (item.startsWith('http')) {
               item = `https://adkdream.top/otherSource/${item}`
+              count++
               return item
             }
             return item
           })
           const newblob = new Blob([newarr.join('\n')], { type: 'text/plain' })
           const newurl = URL.createObjectURL(newblob as Blob)
-          resolve(newurl)
+          if (count === 0) {
+            resolve(videoUrl)
+          } else {
+            resolve(newurl)
+          }
         }
       })
     } catch (error) {
